b38e5223eecf11c7d83826f7a6090b22e7ef8264
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-08-19 01:17:48 -0400

    feat: add index page with navigation (#2)

---
+++ cypress/integration/spec.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+describe("11ty", () => {
+  it("navigates", () => {
+    // find more Cypress commands at
+    // https://on.cypress.io/api
+    cy.visit("/");
+    cy.contains("Hi there");
+    cy.contains("a", "the README").click();
+    cy.location("pathname").should("include", "/README");
+    cy.go("back");
+    cy.contains("Hi there"); // back on the index page
+  });
+});
