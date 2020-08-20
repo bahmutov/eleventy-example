cb29554b9f2485db4fc1091389823b507f261587
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-08-20 12:05:39 -0400

    add link to the blog post

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # My site
+
 > A static site using [11ty](https://www.11ty.dev/)
+
+Read [Test the Preview Vercel Deploys](https://glebbahmutov.com/blog/)
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -7,6 +7,8 @@ describe("11ty", () => {
     cy.contains("Hi there");
     cy.contains("a", "the README").click();
     cy.location("pathname").should("include", "/README");
+    // the link to the blog post
+    cy.contains("a", "Test the Preview Vercel Deploys");
     cy.go("back");
     cy.contains("Hi there"); // back on the index page
   });
